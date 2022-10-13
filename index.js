c896b347afa789745dad1f788cb6abcd082fac99
felipematheus1337 <lipehbr@gmail.com>
2022-10-13 20:51:48 -0300

    bodyparser, routes, and more added

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser")
 
 
 // Estou dizendo para o express usar o EJS como View Engine
 app.set("view engine","ejs");
 app.use(express.static('public'))
 
+//Body parser
+app.use(bodyParser.urlencoded({extended: false})); //tradução para estrutura Javascript
+app.use(bodyParser.json());
+
+
+//Rotas
 app.get("/",(req,res) => {
    
     res.render('index');
@@ -15,6 +22,15 @@ app.get("/",(req,res) => {
 app.get("/perguntar",(req,res) => {
     res.render("perguntar")
 })
+
+
+app.post("/salvarpergunta",(req,res) => {
+
+   var titulo = req.body.titulo;
+   var descricao = req.body.descricao
+   res.send("Formulário recebido! título " + titulo + " " + "descricao" + descricao);
+   //res.status(201).send(titulo);
+});
 
 
 app.listen(8080,(e) => {
